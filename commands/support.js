39a38ab4bb26c9bb9783808ad15a21969a25886c
coding777e <168589076+coding777e@users.noreply.github.com>
2024-05-18 19:38:15 +0200

    6support.js

--- commands/support.js
+++ commands/support.js
@@ -40,7 +40,7 @@ module.exports = {
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
               url: 'https://discord.gg/2nkutxc9hK'
           })
-            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n➡️ **Follow us on:**\n- GitHub - ${githubLink}\n- Replit - ${replitLink}\n- YouTube - ${youtubeLink}`)
+            .setDescription(`➡️ **Join our Discord server for support and updates:**\n- Discord - ${supportServerLink}\n\n}`)
             .setImage('https://cdn.discordapp.com/attachments/1241154542758920334/1241443347499974778/desktop-wallpaper-anime-city-lights-night-rain-umbrella-sky-artist.jpg?ex=664a37b6&is=6648e636&hm=da6eff6241d35bfc984ee7837b9a4694ab30026d39c49739e5345e542a6e1b70&')
             .setTimestamp();
 
